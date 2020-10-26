c2bc30989fcde81c14b615a19eb766ab8f5fb26d
albert88jane <73338253+albert88jane@users.noreply.github.com>
2020-10-26 12:33:42 +0100

    Update coockie.php

--- coockie.php
+++ coockie.php
@@ -1,17 +1,30 @@
 <?php
-
-
-	//Crear coockie per a veure si ja ha demanat el client
+// Your code here!
 
 
 
+   
+        
         $cookie_nombre = "comprobacio";
         $cookie_valor = $nom_usuari;
         $cookie_expira =  getdate($data = time());
         setcookie($cookie_name, $cookie_value, $cookie_expira['mday'] + 1);
+        
+        
+       
+    
 
-
-
-
-
+    
 ?>
+
+<?php
+
+
+        
+       if (isset($_COOKIE["comprobacio"])) {
+         list($cookie_value, $cookie_expira['mday' + 1]) = explode("|", $_COOKIE["comprobacio"]);
+}
+        
+
+    
+    ?>
